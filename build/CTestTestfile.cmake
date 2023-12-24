19bbe57cfbd813555f8db5dcdcf7ecc9430bff59
chathamabate <chathamabate@gmail.com>
2023-12-23 22:51:29 -0500

    Clean up

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chathamabate/Desktop/Git-Workspaces/ti-doc
-# Build directory: /Users/chathamabate/Desktop/Git-Workspaces/ti-doc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
