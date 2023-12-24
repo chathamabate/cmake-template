63ad6fd83f3bf0177754c523347ffa49edabf0ef
chathamabate <chathamabate@gmail.com>
2023-12-23 22:45:59 -0500

    CMake Template Start

---
+++ build/CMakeFiles/TiDoc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TiDoc.dir/src/main.cpp.o"
+  "CMakeFiles/TiDoc.dir/src/main.cpp.o.d"
+  "TiDoc"
+  "TiDoc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TiDoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/chathamabate/Desktop/Git-Workspaces/ti-doc
+# Build directory: /Users/chathamabate/Desktop/Git-Workspaces/ti-doc/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
